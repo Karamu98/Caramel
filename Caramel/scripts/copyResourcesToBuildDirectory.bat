e4118069a7b48853b055c340e9f252fad7177f1e
Karamu98 <callumhaines98@hotmail.co.uk>
2019-05-17 04:26:24 +0100

    Project Settings
    Added build directories for both Debug and Release. Release's script needs to copy assimp lib release version to outdir.

--- Caramel/scripts/copyResourcesToBuildDirectory.bat
+++ Caramel/scripts/copyResourcesToBuildDirectory.bat
@@ -1,2 +1,2 @@
-xcopy "%~dp0..\resources" "%~dp0..\bin" /s /y /i /c
-xcopy "%~dp0..\lib\assimp-vc140-mt.dll" "%~dp0..\bin" /y /c
+xcopy "%~dp0..\resources" "%~dp0..\bin\Debug" /s /y /i /c
+xcopy "%~dp0..\lib\assimp-vc140-mt.dll" "%~dp0..\bin\Debug" /y /c
---
+++ Caramel/scripts/copyResourcesToBuildDirectoryRel.bat
@@ -0,0 +1,2 @@
+xcopy "%~dp0..\resources" "%~dp0..\bin\Release" /s /y /i /c
+xcopy "%~dp0..\lib\assimp-vc140-mt.dll" "%~dp0..\bin\Release" /y /c
